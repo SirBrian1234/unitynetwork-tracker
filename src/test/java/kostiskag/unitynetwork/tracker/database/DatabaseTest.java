06dfe1d2b0df0ca63edd7e0f322598a3cd9620d2
kwsths kag <kwsths kag>
2017-04-05 22:10:18 +0300

    multiple tests

---
+++ src/test/java/kostiskag/unitynetwork/tracker/database/DatabaseTest.java
@@ -0,0 +1,11 @@
+package kostiskag.unitynetwork.tracker.database;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class DatabaseTest {
+	@Test
+	public void evaluatesExpression() {
+	   assertTrue(true);
+	}
+}
