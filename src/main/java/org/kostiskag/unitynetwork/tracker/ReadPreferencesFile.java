42314cde8f867c4fc731d63bcc0f3862ae416991
kostis <kostiskag.gmail.com>
2019-06-26 01:20:50 +0300

    proper str handling on file prefs read

--- src/main/java/org/kostiskag/unitynetwork/tracker/ReadPreferencesFile.java
+++ src/main/java/org/kostiskag/unitynetwork/tracker/ReadPreferencesFile.java
@@ -1,17 +1,17 @@
 package org.kostiskag.unitynetwork.tracker;
 
+import java.util.Properties;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 
 /**
  *
  * @author Konstantinos Kagiampakis
  */
-public class ReadPreferencesFile {
+final class ReadPreferencesFile {
 
 	//These are the imported settings from file
 	public String netName;
@@ -26,102 +26,107 @@ public class ReadPreferencesFile {
 	public boolean gui = true;
 	public boolean log;
 
-	public ReadPreferencesFile() {
+	private ReadPreferencesFile() {
 	}
 
 	public static ReadPreferencesFile ParseFile(InputStream file) throws IOException {
-        
-        Properties cfg = new java.util.Properties();
+
+		ReadPreferencesFile pref = new ReadPreferencesFile();
+
+        Properties cfg = new Properties();
         cfg.load(file);
-       
-        String networkName = cfg.getProperty("NetworkName").replaceAll("\\s+","");
-        String authPort = cfg.getProperty("AuthPort").replaceAll("\\s+","");        
-        String databaseUrl = cfg.getProperty("DatabaseUrl").replaceAll("\\s+","");
-        String databaseUser = cfg.getProperty("DatabaseUser").replaceAll("\\s+","");
-        String databsePassword = cfg.getProperty("DatabsePassword").replaceAll("\\s+","");
-        String blueNodeCapacity = cfg.getProperty("BlueNodeCapacity").replaceAll("\\s+","");     
-        String ping = cfg.getProperty("Ping").replaceAll("\\s+","");        
-        String useGUI = cfg.getProperty("UseGUI").replaceAll("\\s+","");
-        String log = cfg.getProperty("Log").replaceAll("\\s+","");  
 
-        ReadPreferencesFile pref = new ReadPreferencesFile();
-
-		pref.netName = networkName;
-		pref.auth = Integer.parseInt(authPort);
-		pref.databaseUrl = databaseUrl;
-		pref.user = databaseUser;
-		pref.password = databsePassword;
-		pref.bncap = Integer.parseInt(blueNodeCapacity);
-		pref.pingTime = Integer.parseInt(ping);
-		pref.gui = Boolean.parseBoolean(useGUI);
-		pref.log = Boolean.parseBoolean(log);
+		pref.netName = cfg.getProperty("NetworkName").strip();
+		pref.auth = Integer.parseInt(cfg.getProperty("AuthPort").strip());
+		pref.databaseUrl = cfg.getProperty("DatabaseUrl").strip();
+		pref.user = cfg.getProperty("DatabaseUser").strip();
+		pref.password = cfg.getProperty("DatabsePassword").strip();
+		pref.bncap = Integer.parseInt(cfg.getProperty("BlueNodeCapacity").strip());
+		pref.pingTime = Integer.parseInt(cfg.getProperty("Ping").strip());
+		pref.gui = Boolean.parseBoolean(cfg.getProperty("UseGUI").strip());
+		pref.log = Boolean.parseBoolean(cfg.getProperty("Log").strip());
 
         return pref;
     }
 
 	public static void GenerateFile(File file) throws IOException {
 		PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8);
-	    writer.print(""
-		+ "#############################################\n"
-		+ "#         Unity Tracker Config File         #\n"
-		+ "#############################################\n"
-		+ "\n"
-		+ "#\n"
-		+ "# Insructions for setting up the config file\n"
-		+ "#\n"
-		+ "# Do not comment any variable nor remove any from this file as this will result\n"
-		+ "# in an application error. Change the value to an appropriate input as described\n"
-		+ "# instead. If this file gets messed up, you may delete it and it will be\n"
-		+ "# auto-generated from the app in its next run.\n"
-		+ "#\n"
-		+ "\n"
-		+ "#\n"
-		+ "# Network and Tracker Settings\n"
-		+ "#\n"
-		+ "# First of all what shall be the name of the netwrok?\n"
-		+ "# Provide a TCP auth port as well. The default is 8000.\n"
-		+ "NetworkName = UnityNetwork\n"
-		+ "AuthPort = 8000\n"
-		+ "\n"
-		+ "#\n"
-		+ "# Database Settings\n"
-		+ "#\n"
-		+ "# the url should be in this type of form for mysql\n"
-		+ "# DatabaseUrl = jdbc:mysql://IPaddress:port/database\n"
-		+ "# DatabaseUser = username\n"
-		+ "# DatabsePassword = password\n"
-		+ "#\n"
-		+ "# the url should be in this type of form for sqlite\n"
-		+ "# DatabaseUrl = jdbc:sqlite:local_database_file.db\n"
-		+ "#\n"
-		+ "DatabaseUrl = jdbc:sqlite:unity.db\n"
-		+ "DatabaseUser = username\n"
-		+ "DatabsePassword = password\n"
-		+ "\n"
-		+ "#\n"
-		+ "# Load and Capacity\n"
-		+ "# Leave zero for an infinite number of connected Blue Nodes\n"
-		+ "# otherwise set a maximum limit, ex. 100.\n"
-		+ "#\n"
-		+ "BlueNodeCapacity = 0\n"
-		+ "\n"
-		+ "# ping time in sec\n"
-		+ "# ping is the time gap where the tracker may search for all active Blue Nodes\n"
-		+ "# in order to detect whether someone is not responding\n"
-		+ "Ping = 180\n"
-		+ "\n"
-		+ "#\n"
-		+ "# Application behaviour\n"
-		+ "#\n"
-		+ "\n"
-		+ "# set GUI or command line\n"
-		+ "# use true or false\n"
-		+ "UseGUI = true\n"
-		+ "\n"
-		+ "# Logging in tracker.log\n"
-		+ "# use true or false\n"
-		+ "Log = true\n"
-	    + "");
+	    writer.print(String.join("\n",
+				"#############################################",
+		"#         Unity Tracker Config File         #",
+		"#############################################",
+		"",
+		"#",
+		"# Insructions for setting up the config file",
+		"#",
+		"# Do not comment any variable nor remove any from this file as this will result",
+		"# in an application error. Change the value to an appropriate input as described",
+		"# instead. If this file gets messed up, you may delete it and it will be",
+		"# auto-generated from the app in its next run.",
+		"#",
+		"",
+		"#",
+		"# Network and Tracker Settings",
+		"#",
+		"# First of all what shall be the name of the netwrok?",
+		"# Provide a TCP auth port as well. The default is 8000.",
+		"NetworkName = UnityNetwork",
+		"AuthPort = 8000",
+		"",
+		"#",
+		"# Database Settings",
+		"#",
+		"# the url should be in this type of form for mysql",
+		"# DatabaseUrl = jdbc:mysql://IPaddress:port/database",
+		"# DatabaseUser = username",
+		"# DatabsePassword = password",
+		"#",
+		"# the url should be in this type of form for sqlite",
+		"# DatabaseUrl = jdbc:sqlite:local_database_file.db",
+		"#",
+		"DatabaseUrl = jdbc:sqlite:unity.db",
+		"DatabaseUser = username",
+		"DatabsePassword = password",
+		"",
+		"#",
+		"# Load and Capacity",
+		"# Leave zero for an infinite number of connected Blue Nodes",
+		"# otherwise set a maximum limit, ex. 100.",
+		"#",
+		"BlueNodeCapacity = 0",
+		"",
+		"# ping time in sec",
+		"# ping is the time gap where the tracker may search for all active Blue Nodes",
+		"# in order to detect whether someone is not responding",
+		"Ping = 180",
+		"",
+		"#",
+		"# Application behaviour",
+		"#",
+		"",
+		"# set GUI or command line",
+		"# use true or false",
+		"UseGUI = true",
+		"",
+		"# Logging in tracker.log",
+		"# use true or false",
+		"Log = true",
+	    ""));
 	    writer.close();		
-	}   
+	}
+
+	@Override
+	public String toString() {
+		return String.join("\n","ReadPreferencesFile{",
+				"netName='" + netName + "\',",
+				"auth=" + auth +",",
+				"databaseUrl='" + databaseUrl + "\',",
+				"user='" + user + "\',",
+				"password='" + password + "\',",
+				"bncap=" + bncap + ",",
+				"pingTime=" + pingTime + ",",
+				"gui=" + gui + ",",
+				"log=" + log + ",",
+				"}");
+	}
 }
---
+++ src/test/java/org/kostiskag/unitynetwork/tracker/ReadPreferencesTest.java
@@ -0,0 +1,37 @@
+package org.kostiskag.unitynetwork.tracker;
+
+import org.junit.Test;
+import org.junit.After;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReadPreferencesTest {
+  @Test
+  public void evaluatesExpression() throws IOException {
+    var name = "testfile";
+    var file = new File(name);
+    ReadPreferencesFile.GenerateFile(file);
+
+    var filein = new FileInputStream(file);
+    var r  = ReadPreferencesFile.ParseFile(filein);
+
+    assertEquals("UnityNetwork", r.netName);
+    assertEquals(8000, r.auth);
+    assertEquals("jdbc:sqlite:unity.db", r.databaseUrl);
+    assertEquals("username", r.user);
+    assertEquals("password", r.password);
+    assertEquals(0, r.bncap);
+    assertEquals(180, r.pingTime);
+    assertEquals(true, r.gui);
+    assertEquals(true, r.log);
+    //System.out.println(r);
+  }
+
+  @After
+  public void deleteFile() {
+    var f = new File("testfile");
+    f.delete();
+  }
+}
